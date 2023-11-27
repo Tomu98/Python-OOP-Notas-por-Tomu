71e28ef106aa0d6c07e4792a7c83772e181381e8
Abel Tomás <abeltomasr98@gmail.com>
2023-11-26 22:07:47 -0300

    Correcciones en '2.1_encapsulacion'

--- 2- Encapsulamiento/2.1_encapsulacion.py
+++ 2- Encapsulamiento/2.1_encapsulacion.py
@@ -1,23 +1,26 @@
-# --- ENCAPSULACIÓN ---
-
-
+# ENCAPSULACIÓN
 # Es la capacidad de una clase de ocultar sus detalles internos (atributos y métodos) y exponer una interfaz pública bien definida para interactuar con los objetos de esa clase.
 # El encapsulamiento se logra mediante el uso de atributos y métodos de acceso.
 
-# La encapsulación tiene varios objetivos importantes.
-# - Ocultamiento de la complejidad: Permite ocultar los detalles de implementación de una clase, lo que facilita el uso de la clase sin preocuparse por cómo funciona internamente.
-# - Protección de datos: Evita que los datos de un objeto se modifiquen directamente desde fuera de la clase sin un control adecuado.
-# - Facilita el mantenimiento del código: Si los detalles internos de una clase cambian, los cambios solo deben realizarse en la clase, sin afectar el código que utiliza objetos de esa clase.
-# - Fomenta el principio de "bajo acoplamiento": Reduce la dependencia entre las clases, ya que una clase no necesita conocer los detalles internos de otra clase para interactuar con ella.
+# La encapsulación tiene varios objetivos importantes:
+# Una de ellas es ocultar los detalles de implementación de una clase, facilitando el uso de la clase sin preocuparse por cómo funciona internamente.
+# Otro objetivo es evitar que los datos de un objeto se modifiquen directamente desde fuera de la clase sin un control adecuado.
+# También facilita el mantenimiento del código, si los detalles internos de una clase cambian, los cambios solo deben realizarse en la clase, sin afectar el código que utiliza objetos de esa clase.
+# Y por ultimo fomenta el principio de "bajo acoplamiento", que reduce la dependencia entre las clases, ya que una clase no necesita conocer los detalles internos de otra clase para interactuar con ella.
 
-# Atributos protegidos con un guión bajo ( _ ):
+
+
+# Atributos protegidos con un guión bajo ( _atributo )
 # Al utilizar un guión bajo al principio de un atributo (por ejemplo, _nombre), se indica que el atributo es considerado "protegido".
-# El atributo sigue siendo accesible desde fuera de la clase, se supone que los desarrolladores no deben modificarlo directamente.
+# En su lugar, se debe utilizar un método de acceso para obtener o modificar el valor del atributo.
+# Esto es más una convención que una regla, ya que Python no impide que se acceda o modifique el atributo directamente, por lo que sigue siendo posible hacerlo.
+# Sin embargo, si se utiliza un método de acceso, se puede agregar lógica adicional para controlar el acceso al atributo.
 
-# Atributos privados con dos guiones bajos ( __ ):
+# Atributos privados con dos guiones bajos ( __atributo )
 # Al utilizar dos guiones bajos al principio de un atributo (por ejemplo, __edad), se indica que el atributo es "privado".
 # Con eso se aplica un mecanismo de "name mangling" para hacer que el nombre del atributo sea más difícil de acceder desde fuera de la clase.
 # Esto no lo hace completamente inaccesible, pero hace que sea menos probable que se acceda accidentalmente.
+# Al igual que con los atributos protegidos, se debe utilizar un método de acceso para obtener o modificar el valor del atributo.
 
 # Ejemplos de ambos atributos:
 class Private:
@@ -41,3 +44,8 @@ objeto.cambiar_atributo_privado("nuevo_valor")
 print(objeto.obtener_atributo_privado())  # nuevo_valor
 
 # La encapsulación real depende de la responsabilidad del desarrollador para no acceder ni modificar directamente los atributos protegidos o privados desde fuera de la clase.
+# Sin embargo, si se hace, el uso de métodos de acceso facilita la tarea de modificar la clase en el futuro, ya que se puede agregar lógica adicional en los métodos de acceso para controlar el acceso a los atributos.
+# Por ejemplo, se puede agregar una condición para verificar que el nuevo valor de un atributo sea válido antes de asignarlo.
+# Si se accede directamente al atributo, no se puede agregar esta lógica adicional.
+# Por lo tanto, es una buena práctica utilizar métodos de acceso para todos los atributos protegidos y privados, incluso si no se agrega lógica adicional en el momento.
+# De esta manera, si se necesita agregar lógica adicional en el futuro, se puede hacer sin tener que modificar el código que utiliza la clase.
