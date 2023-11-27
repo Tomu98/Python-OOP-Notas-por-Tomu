5301639b77217e4c21f417ff12bbf59a4fe54001
Abel Tomás <abeltomasr98@gmail.com>
2023-11-26 22:39:04 -0300

    Correcciones en '3.3_mro'

--- 3- Herencia/3.3_mro.py
+++ 3- Herencia/3.3_mro.py
@@ -1,21 +1,19 @@
-# --- MRO ---
-
-# "Method Resolution Order" o "Orden de Resolución de Métodos".
+# MRO ("Method Resolution Order" o "Orden de Resolución de Métodos").
 # Se utiliza para determinar el orden en el que se buscan y ejecutan los métodos de una clase en una jerarquía de herencia múltiple.
-# La MRO es crucial para garantizar que los métodos se llamen en el orden correcto y que las clases derivadas hereden y sobrescriban correctamente los métodos de las clases base.
+# Es crucial para garantizar que los métodos se llamen en el orden correcto y que las clases derivadas hereden y sobrescriban correctamente los métodos de las clases base.
+
 
 # Problema de la Ambigüedad
 # Cuando una subclase hereda de múltiples superclases, puede surgir un problema de ambigüedad si ambas superclases tienen un método con el mismo nombre.
 # Aquí es donde entra en juego la MRO para resolver este conflicto.
 
+
 # El C3 Linearization Algorithm
 # Python utiliza el algoritmo de linearización C3 para calcular la MRO.
 # Este algoritmo sigue un conjunto de reglas para determinar el orden en el que se buscan los métodos en la jerarquía de clases.
 # Las reglas son las siguientes:
-# - Depth-First Search (DFS): El algoritmo sigue una estrategia de búsqueda en profundidad para recorrer la jerarquía de clases.
-#   Esto significa que primero busca en la clase actual antes de pasar a las superclases.
-# - Left-to-Right: Si una subclase hereda de varias superclases, se mantiene el orden en el que se mencionaron las superclases en la declaración de la subclase.
-#   Python sigue este orden al calcular la MRO.
+# - Depth-First Search (DFS): el algoritmo sigue una estrategia de búsqueda en profundidad para recorrer la jerarquía de clases, significa que primero busca en la clase actual antes de pasar a las superclases.
+# - Left-to-Right: Si una subclase hereda de varias superclases, se mantiene el orden en el que se mencionaron las superclases en la declaración de la subclase. Python sigue este orden al calcular la MRO.
 # - Consistencia: El algoritmo C3 garantiza que el orden de resolución sea consistente y no dependa de la forma en que se definieron las clases.
 
 # Ejemplo para ilustrar MRO:
@@ -71,7 +69,6 @@ d.saludo()
 # En este ejemplo, la clase D hereda de B y C, ambas de A.
 # Cuando llamamos a "d.saludo()", Python sigue la MRO y ejecuta primero el método "saludo" de A utilizando "super()", luego ejecuta el método saludo de C (también utilizando "super()"), y finalmente ejecuta el método saludo de B.
 
-
 # En algunos casos, puede ocurrir un conflicto en la MRO, especialmente cuando hay ambigüedad en la jerarquía de clases.
 # Cuando esto sucede, Python generará un error de tipo "TypeError" para evitar comportamientos ambiguos.
 
@@ -87,13 +84,16 @@ print(mro_result1)
 # En este ejemplo, la tupla muestra el orden de resolución de métodos para la clase "D". 
 # Python buscará métodos en "D", luego en "B", luego en "C", luego en "A", y finalmente en la clase base "object".
 
+
 # "__mro__"
 # Es un atributo especial en Python que está presente en todas las clases y es una representación más directa de la MRO de una clase.
 # En lugar de llamar al método "mro()", puedes acceder al atributo "__mro__" directamente desde la clase.
+# Ejemplo:
 mro_result2 = D.__mro__
 print(mro_result2)
 # Saldrá lo mismo que el anterior ejemplo:
 # "(<class '__main__.D'>, <class '__main__.B'>, <class '__main__.C'>, <class '__main__.A'>, <class 'object'>)"
+
 
 # Tanto "mro()" como "__mro__" proporcionan información sobre el orden de resolución de métodos para una clase y su jerarquía de clases.
 # "mro()" es un método que devuelve una lista, mientras que "__mro__" es un atributo que almacena la misma información de manera más directa.
@@ -101,6 +101,6 @@ print(mro_result2)
 
 
 # Conclusiones
-# La MRO (Method Resolution Order) es un concepto fundamental en Python que resuelve el problema de la ambigüedad cuando una subclase hereda de múltiples superclases.
+# La MRO (Method Resolution Order) es un concepto fundamental que resuelve el problema de la ambigüedad cuando una subclase hereda de múltiples superclases.
 # Python utiliza el algoritmo de linearización C3 para calcular el orden en el que se buscan los métodos en la jerarquía de clases.
 # Esto asegura un comportamiento consistente y predecible en la resolución de métodos, lo que es esencial para POO en Python.
